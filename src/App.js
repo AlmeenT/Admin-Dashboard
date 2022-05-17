657075a4f7134d6570e1f7d69a31f2d6649dab1b
Kandy-Hamisi <hkandy2000@gmail.com>
2022-05-17 09:34:55 +0300

    :hammer: Added a homepage containing navbar and sidebar. :heavy_plus_sign: added the react icons dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+// import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-        <Login />
+        {/* <Login /> */}
+        <Home />
     </div>
   );
 }
