b3c7338251279e99d0d10e6a6a4f4796492d1293
AlmeenT <tiamiyualameen@ymail.com>
2022-05-16 23:29:12 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './Pages/Login/Home/Home';
 import Login from './Pages/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
-        <Login />
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/home" element={ <Home />}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,24 +1,47 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './Login.css'
+import {Container, Row, Col, Button, Form} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function Login() {
-  return (
-    <div className='login'> 
-        <div className='loginInfo' >
-            <h1> SH 75 DASHBOARD </h1>
-            <p> Login as an admin to view your dashboard </p>
-        </div>
-        <div>
-            <form className='loginForm '>
-                    <label> Email </label>
-                    <input type='email' placeholder='Enter you email' />
-                
-                    <label> Password </label>
-                    <input type='text' placeholder='Enter your password' />
 
-                <div><button> Log in </button></div>
-            </form>
-        </div>
+
+const submitHandler = (e) => {
+    if (
+      Form.Control !==''
+    ){
+      e.preventDefault()
+      window.location="/Home"
+    } else {
+      window.location='/'
+    }
+
+}
+
+  return (
+    <div className='login'>
+      <Container>
+        <Row>
+          <Col style={{ marginTop: 200 }} >
+            <h1> SH-75 Admin </h1>
+            <p> Login to access your dashboard </p>          
+          </Col>
+          <Col style={{ marginTop: 200 }}>
+            <Form onSubmit={submitHandler} >
+              <Form.Group controlId='formEmail' className='mb-3'>
+                <Form.Label> Email Address </Form.Label>
+                <Form.Control type='email' placeholder='Example@email.com' ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId='formPassword' className='mb-3' >
+                <Form.Label> Password </Form.Label>
+                <Form.Control type='password' placeholder='enter your password' ></Form.Control>
+              </Form.Group>
+              <Button variant='secondary' type='submit' > Log in </Button>
+            </Form>
+          </Col>
+        </Row> 
+      </Container>
     </div>
   )
 }
